493add68931a83afa2202c2e3288826ce5fc60c5
Md. Razwanul Hasan <razwanulhasan@gmail.com>
2023-03-22 12:18:20 +0600

    Complete Doctor Auth

---
+++ AppointmentRx.DataAccess/Repositories/Doctor/Profile/IDoctorProfileRepository.cs
@@ -0,0 +1,16 @@
+﻿using AppointmentRx.DataAccess.Entitites;
+using AppointmentRx.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentRx.DataAccess.Repositories.Doctor.Profile
+{
+    public interface IDoctorProfileRepository
+    {
+        public Task<DoctorProfile?> GetById(string id);
+        public Task<HttpResponseModel> Create(DoctorProfile entity);
+    }
+}
